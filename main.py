845bc61da7e1a5d7b055d3fe0a875de209a86205
Azadova Indira <aindira2197@gmail.com>
2026-04-17 14:54:58 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+CREATE TABLE employees (
+    id INT,
+    department VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees VALUES
+(1, 'IT', 4000),
+(2, 'IT', 3500),
+(3, 'HR', 2500);
+
+SELECT department, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department;
